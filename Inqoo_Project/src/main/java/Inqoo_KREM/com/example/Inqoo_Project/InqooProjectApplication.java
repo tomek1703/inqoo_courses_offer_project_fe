a1487cf004694ca9b47cff28fc783ae5386d54d4
mmuszol <mmuszol@gmail.com>
2022-07-16 11:09:22 +0200

    Backend_initiation

---
+++ Inqoo_Project/src/main/java/Inqoo_KREM/com/example/Inqoo_Project/InqooProjectApplication.java
@@ -0,0 +1,13 @@
+package Inqoo_KREM.com.example.Inqoo_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InqooProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InqooProjectApplication.class, args);
+	}
+
+}
---
+++ Inqoo_Project/src/test/java/Inqoo_KREM/com/example/Inqoo_Project/InqooProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Inqoo_KREM.com.example.Inqoo_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InqooProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
