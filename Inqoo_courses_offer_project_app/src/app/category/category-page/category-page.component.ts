6e2bddbf9eb036073e8df4bac652a7eb24c4762a
jakubwolowiec <97116305+jakubwolowiec@users.noreply.github.com>
2022-07-30 11:10:27 +0200

    Finished mocking tooltip, tiles. Changed Inqoo logo for white ver.

--- Inqoo_courses_offer_project_app/src/app/category/category-page/category-page.component.ts
+++ Inqoo_courses_offer_project_app/src/app/category/category-page/category-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MockCategories} from "../../shared/MockCategories";
 
 @Component({
   selector: 'app-category-page',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./category-page.component.scss']
 })
 export class CategoryPageComponent{
+public items: MockCategories[] = [{
+  id: 0,
+  title: "IT",
+  description: "IT Desc"
 
+},
+  {
+    id: 1,
+    title: "Sales",
+    description: "sales desc"
+  },
+  {
+    id: 2,
+    title: "Marketing",
+    description: "marketing desc"
+  },
+  {
+    id: 3,
+    title: "Other",
+    description: "other desc"
+  }];
 }
