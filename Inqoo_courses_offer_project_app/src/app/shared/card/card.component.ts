b2be334973e7e1533ca441d26ce5e9cff4abd159
jakubwolowiec <97116305+jakubwolowiec@users.noreply.github.com>
2022-07-30 09:58:03 +0200

    Finished mocking tooltip

--- Inqoo_courses_offer_project_app/src/app/shared/card/card.component.ts
+++ Inqoo_courses_offer_project_app/src/app/shared/card/card.component.ts
@@ -7,4 +7,5 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 })
 export class CardComponent{
   @Input() title: String = "Test";
+  @Input() description: String = "Test description";
 }
