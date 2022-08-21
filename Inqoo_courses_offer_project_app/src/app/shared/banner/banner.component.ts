e6d81b9c6fc09381bd7fc4a6b53d068c4fa11226
jakubwolowiec <97116305+jakubwolowiec@users.noreply.github.com>
2022-08-21 13:53:04 +0200

    Fixed requested issues

--- Inqoo_courses_offer_project_app/src/app/shared/banner/banner.component.ts
+++ Inqoo_courses_offer_project_app/src/app/shared/banner/banner.component.ts
@@ -1,12 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss']
 })
-export class BannerComponent  {
-  // bgImageVariableBanner = 'url("../../../assets/home-page-picture.jpg")';
-
-
+export class BannerComponent {
 }
