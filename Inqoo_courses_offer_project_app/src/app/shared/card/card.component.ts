a5af0a489b9e84fd1488ac17c3cfb803082e66ab
jakubwolowiec <97116305+jakubwolowiec@users.noreply.github.com>
2022-08-21 09:42:33 +0200

    Tried to make bg-images dynamic

--- Inqoo_courses_offer_project_app/src/app/shared/card/card.component.ts
+++ Inqoo_courses_offer_project_app/src/app/shared/card/card.component.ts
@@ -8,6 +8,4 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 export class CardComponent{
   @Input() title: String = "Test";
   @Input() description: String = "Test description";
-
-  bgImageVariable = 'url("../../../assets/card1.jpg")';
 }
