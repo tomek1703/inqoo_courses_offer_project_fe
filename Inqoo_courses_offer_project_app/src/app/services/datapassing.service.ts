d9de97325d6c95dad3080456decc375c59149bf0
jakubwolowiec <97116305+jakubwolowiec@users.noreply.github.com>
2022-09-25 14:13:31 +0200

    Added passing subcategories data

--- Inqoo_courses_offer_project_app/src/app/services/datapassing.service.ts
+++ Inqoo_courses_offer_project_app/src/app/services/datapassing.service.ts
@@ -7,9 +7,15 @@ import {SubCategory} from "../models/SubCategory";
 })
 export class DatapassingService {
 private coursesState = new BehaviorSubject(Array<SubCategory>());
+private uuid = new BehaviorSubject(String());
+currentUUIDState = this.uuid.asObservable();
 currentCoursesState = this.coursesState.asObservable();
 
 updateCoursesState(courses: Array<SubCategory>){
   this.coursesState.next(courses);
+}
+
+updateUUIDState(uuid: string){
+  this.uuid.next(uuid);
 }
 }
